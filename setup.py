f8f2bd04404403e153327bade48d5f459585d811
Jesus Vedasto A. Olazo <jestoy.olazo@gmail.com>
2017-08-14 12:26:24 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'prtp',
+  packages = ['prtp'],
+  version = '1.0',
+  description = 'A simple module for printing files to printer using Tkinter and PyWin32',
+  author = 'Jesus Vedasto Olazo',
+  author_email = 'jessie@jestoy.frihost.net',
+  url = 'https://github.com/jestoy0514/prtp',
+  download_url = 'https://github.com/jestoy0514/prtp/archive/1.0.tar.gz',
+  keywords = ['gui', 'print', 'example'],
+  classifiers = [],
+)
